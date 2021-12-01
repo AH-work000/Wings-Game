9796c57d589a5c6896485a44fcfbdb156eb03204
AH-work000 <70098254+AH-work000@users.noreply.github.com>
2021-12-01 20:03:00 +1100

    Movement-Range-Added

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -8,6 +8,8 @@ public class InputManager : MonoBehaviour
 
     public SpriteRenderer hotsauceSprayRenderer;
 
+    public Camera mainCamera;
+
     // Member Variables for all variation of the Hotsauce Spray Sprite
 
     [SerializeField]
@@ -19,23 +21,37 @@ public class InputManager : MonoBehaviour
     [SerializeField]
     private Sprite hotSauceSprayRightPos;
 
-    // Member Variables for Hotsauce Spray Movement
+    // Member Variables for Hotsauce Spray Machine
     private Vector3 movement;
+
+    private float hotSauceSprayXPos;
+
+    private Vector3 hotsauceScreenPos;
 
     // Start is called before the first frame update
     void Start()
     {
+        hotSauceSprayXPos = hotsauceSprayRenderer.transform.position.x;
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Debug Section
+
         // Print out the Time for each frame -- To be commented out once the game is completed
-        Debug.Log("Time DeltaTime: " + Time.deltaTime);
+        // Debug.Log("Time DeltaTime: " + Time.deltaTime);
 
         // Print out the Time for the duration of the game -- To be commented out once the game is completed
-        Debug.Log("Time since start game: " + Time.time);
+        // Debug.Log("Time since start game: " + Time.time);
+
+        // Print out properties info regarding the Main Camera
+            Debug.Log("Screen Position of the Hotsource Spray" + hotsauceScreenPos);
+
+
+        // Code Section 
+
 
         // Inputs for the rotation of the Hotsauce Spray Machine
 
@@ -43,7 +59,6 @@ public class InputManager : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.E))
             {
                 hotsauceSprayRenderer.sprite = hotSauceSprayRightPos;
-                Debug.Log("Input successful!");
             }
 
             // If the 'Q' Key is pressed --> Rotate the Hotsauce Spray Machine -45 degree (LeftPos)
@@ -59,10 +74,40 @@ public class InputManager : MonoBehaviour
             }
 
         // Get the horizontal axis where it moves 3.5 metres per second
-        movement.x += Input.GetAxis("Horizontal") * 0.5f * Time.deltaTime;
+        movement.x += Input.GetAxis("Horizontal") * 0.08f * Time.deltaTime;
 
-        // The move translation of the Hotsauce Spray Machine along the x axis
-        hotsauceSprayRenderer.transform.Translate(movement, Space.World);
+
+        // Check if the current x pos of the Hotsource Spray Machine is outside
+        // the width range of the Camera.
+        if (CheckIfHotsauceSprayOutOfCamera(hotsauceSprayRenderer.transform))
+        {
+            // The move translation of the Hotsauce Spray Machine along the x axis
+            hotsauceSprayRenderer.transform.Translate(movement, Space.World);
+        }
+
+    }
+
+    // Method to Check if the current x pos of the Hotsource Spray Machine is outside
+    // the width range of the Camera.
+    private bool CheckIfHotsauceSprayOutOfCamera(Transform targetTransform)
+    {
+        // Convert the World Coordinates of the Hotsauce Spray Machine to Screen Coordinates
+        hotsauceScreenPos = mainCamera.WorldToScreenPoint(targetTransform.position);
+
+        // Designate the coordinate of the maximum point (right edge) that the Hotsauce Spray Machine stops at
+        float xPosAtRightEdge = mainCamera.pixelWidth - 100f;
+
+        // Designate the coordinate of the minimum point (left edge) that the Hotsauce Spray Machin stops at
+        float xPosAtLeftEdge = 100f;
+
+
+        if (hotsauceScreenPos.x <= xPosAtRightEdge && hotsauceScreenPos.x >= xPosAtLeftEdge)
+        {
+            return true;
+        }
+
+        return false;
+
     }
 
     /* 
@@ -81,7 +126,5 @@ public class InputManager : MonoBehaviour
         // Do the lerp method
         hotSauceSprayRenderer.transform.position = Vector3.Lerp(oldPos, newPos, timeFraction);
     } */
-
-
 
 }
