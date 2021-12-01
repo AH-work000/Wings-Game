bd474b78ee346e3cd3d78f294e50d68b4e81bce8
AH-work000 <70098254+AH-work000@users.noreply.github.com>
2021-12-01 18:18:44 +1100

    Movement-Axes-Done

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     // Member Variables
-    public SpriteRenderer hotSauceSprayRenderer;
 
-    // Member Variables for all variation of the Hotsauce Spray
+    public SpriteRenderer hotsauceSprayRenderer;
+
+    // Member Variables for all variation of the Hotsauce Spray Sprite
 
     [SerializeField]
     private Sprite hotSauceSprayLeftPos;
@@ -18,11 +19,13 @@ public class InputManager : MonoBehaviour
     [SerializeField]
     private Sprite hotSauceSprayRightPos;
 
+    // Member Variables for Hotsauce Spray Movement
+    private Vector3 movement;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -36,24 +39,49 @@ public class InputManager : MonoBehaviour
 
         // Inputs for the rotation of the Hotsauce Spray Machine
 
+            // If the 'E' Key is pressed --> Rotate the Hotsauce Spray Machine 45 degrees (RightPos)
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                hotsauceSprayRenderer.sprite = hotSauceSprayRightPos;
+                Debug.Log("Input successful!");
+            }
 
-        // If the 'E' Key is pressed --> Rotate the Hotsauce Spray Machine 45 degrees (RightPos)
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            hotSauceSprayRenderer.sprite = hotSauceSprayRightPos;
-        }
+            // If the 'Q' Key is pressed --> Rotate the Hotsauce Spray Machine -45 degree (LeftPos)
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                hotsauceSprayRenderer.sprite = hotSauceSprayLeftPos;
+            }
 
-        // If the 'Q' Key is pressed --> Rotate the Hotsauce Spray Machine -45 degree (LeftPos)
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            hotSauceSprayRenderer.sprite = hotSauceSprayLeftPos;
-        }
+            // If the 'W' Key is pressed --> Rotate the Hotsauce Spray Machine back to its Up Pos
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                hotsauceSprayRenderer.sprite = hotSauceSprayUpPos;
+            }
 
-        // If the 'W' Key is pressed --> Rotate the Hotsauce Spray Machine back to its Up Pos
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            hotSauceSprayRenderer.sprite = hotSauceSprayUpPos;
-        }
+        // Get the horizontal axis where it moves 3.5 metres per second
+        movement.x += Input.GetAxis("Horizontal") * 0.5f * Time.deltaTime;
 
+        // The move translation of the Hotsauce Spray Machine along the x axis
+        hotsauceSprayRenderer.transform.Translate(movement, Space.World);
     }
+
+    /* 
+    // Method for doing the lerp movement
+    private void DoLerp()
+    {
+        // Create a new variable and make it get the current position of the Hotsauce Spray
+        Vector3 oldPos = hotSauceSprayRenderer.transform.position;
+
+        // Create a new variable that stores the end position of the Hotsauce Spray
+        Vector3 newPos = new Vector3(hotSauceSprayRenderer.transform.position.x - 1.0f, hotSauceSprayRenderer.transform.position.y);
+
+        // Divide the current time.deltaTime by 2.0f (Time duration)
+        float timeFraction = Time.deltaTime / TIME_DURATION;
+
+        // Do the lerp method
+        hotSauceSprayRenderer.transform.position = Vector3.Lerp(oldPos, newPos, timeFraction);
+    } */
+
+
+
 }
