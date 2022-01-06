4105928a40991e44fe1c450dd42722331eb66b5c
AH-work000 <70098254+AH-work000@users.noreply.github.com>
2022-01-06 12:43:14 +1100

    Main-Menu-UI-Added

--- Assets/Scripts/LoadManager.cs
+++ Assets/Scripts/LoadManager.cs
@@ -22,38 +22,6 @@ public class LoadManager : MonoBehaviour
         
     }
 
-    /* 
-    // Method: Load the Gameover Scene (Game-Over Pop-Up Window)
-    public void LoadGameOverScene()
-    {
-        // Load the Gameover Scene using Async Loading
-        SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
-    }
-
-
-    // Method: Restart the Entire Gameplay Scene
-    public void RestartGame()
-    {
-        // Unload the Gameover Scene using Async Loading
-        SceneManager.UnloadSceneAsync(2);
-        SceneManager.LoadScene(1);
-    }
-
-
-    // Method: Unload both the Gameover and Gameplay scenes and load the Main Menu
-    public void LoadMainMenu()
-    {
-        // Unload both the Gameover and Gameplay scenes using Async Loading
-        SceneManager.UnloadSceneAsync(2);
-        SceneManager.UnloadSceneAsync(1);
-
-        // Then Load the Main Scene
-        SceneManager.LoadScene(0);
-
-    } */
-
-
-
     // Method: Load a Scene using the LoadSceneAsync Method
     public void LoadScene(SceneMode scene)
     {
